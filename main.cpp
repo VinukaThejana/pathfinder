db934e0ea551e57df6f2b7402cc8ae502131588b
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-08-30 21:26:34 +0530

    feat(init): increased the stack size

--- main.cpp
+++ main.cpp
@@ -5,23 +5,21 @@ TaskHandle_t ReceiveTask;
 
 void send(void * parameter) {
     for (;;) {
-
     }
 }
 
 void receive(void * parameter) {
     for (;;) {
-
     }
 }
 
 void setup() {
     xTaskCreatePinnedToCore(
-        send, "send", 1000, NULL, 1, &SendTask, 0
+        send, "send", 10000, NULL, 1, &SendTask, 0
     );
 
     xTaskCreatePinnedToCore(
-        receive, "receive", 1000, NULL, 1, &ReceiveTask, 1
+        receive, "receive", 10000, NULL, 1, &ReceiveTask, 1
     );
 }
 
