de0153385b887fac6bf6b2e95e18b8c33ec9f530
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-08-30 18:07:49 +0530

    feat(init): initial commit

---
+++ lib/blink.h
@@ -0,0 +1,26 @@
+#ifndef BLINK
+#define BLINK
+
+#include "esp32-hal-gpio.h"
+#include "esp32-hal.h"
+#include <Arduino.h>
+
+// Number of blinks
+#define BLINK_DURATION 5
+// The interval between blinks in miliseconds
+#define BLINK_DELAY 200
+
+inline void blink(
+    int pin,
+    int blink_duration = BLINK_DURATION,
+    int blink_delay = BLINK_DELAY
+) {
+    for (int i = 1; i <= blink_duration; i++) {
+        digitalWrite(pin, HIGH);
+        delay(blink_delay);
+        digitalWrite(pin, LOW);
+        delay(blink_delay);
+    }
+}
+
+#endif // !BLINK
