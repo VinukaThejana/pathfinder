870d724e4a32e69046bec2ace8f60be6db55afc7
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-08-30 18:26:37 +0530

    feat(init): added multithreading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <Arduino.h>
 
+TaskHandle_t SendTask;
+TaskHandle_t ReceiveTask;
+
+void send(void * parameter) {
+    for (;;) {
+    }
+}
+
+void receive(void * parameter) {
+    for (;;) {
+    }
+}
+
 void setup() {
+    xTaskCreatePinnedToCore(
+        send, "send", 1000, NULL, 1, &SendTask, 0
+    );
+
+    xTaskCreatePinnedToCore(
+        receive, "receive", 1000, NULL, 1, &ReceiveTask, 1
+    );
 }
 
 void loop() {
