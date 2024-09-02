5afc9b0b045000f03f4365b78044a2da6fc11a75
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-02 22:46:06 +0530

    feat(receive): added the ability to store the incomming lat and lon

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
+/**
+* Sensor configuration
+*
+*   SD Card reader
+*       - CS   = 5
+*       - MOSI = 23
+*       - MISO = 19
+*       - SCK  = 18
+*
+**/
+
 #include <HardwareSerial.h>
-#include "esp32-hal-gpio.h"
 #include <Arduino.h>
 #include <BluetoothSerial.h>
-#include "lib/blink.h"
-#include "lib/gps.h"
+#include "esp32-hal-gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-#define LED 2
+#include "lib/blink.h"
+#include "lib/defer.h"
+#include "lib/gps.h"
+#include "lib/io.h"
+
 #define RECEIVE_TASK_PRIORITY 2
 #define SEND_TASK_PRIORITY 1
 #define MAX_RECEIVE_LENGTH 100 // Bytes
 #define SERIAL_REFRESH_RATE 9600
 #define BLUETOOTH_NAME "ESP32"
+#define COORD_FILE "/coord"
+
+#define LED 2
+#define SD_CS 5
 
 // FreeRTOS Tasks
 TaskHandle_t SendTask;
@@ -22,12 +41,15 @@ SemaphoreHandle_t mutex;
 void send(void * parameter) {
     for (;;) {
         // TODO: Add the sensors here
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 void receive(void * parameter) {
     char data[MAX_RECEIVE_LENGTH];
     for (;;) {
+        Defer d([]{ vTaskDelay(pdMS_TO_TICKS(10)); });
+
         if (!SerialBT.available()) {
             continue;
         }
@@ -50,18 +72,40 @@ void receive(void * parameter) {
             continue;
         }
 
-        // TODO: use the latitude and the longitude as needed
+        payload = "lat:" + String(coord.lat, 6) + "|lon:" + String(coord.lon, 6);
+        IO::ErrCode err;
+
+        xSemaphoreTake(mutex, portMAX_DELAY);
+        err = IO::write(COORD_FILE, payload.c_str());
+        xSemaphoreGive(mutex);
+        if (err != IO::SUCCESS) {
+            xSemaphoreTake(mutex, portMAX_DELAY);
+            Serial.println(
+                "failed to save the coordinates"
+            );
+            blink(LED);
+            xSemaphoreGive(mutex);
+            continue;
+        }
     }
 }
 
 void setup() {
     pinMode(LED, OUTPUT);
-
     Serial.begin(SERIAL_REFRESH_RATE);
+
     if (!SerialBT.begin(BLUETOOTH_NAME)) {
         for (;;) {
             Serial.println(
                 "bluetooth initialization failed"
+            );
+            blink(LED);
+        }
+    }
+    if (!SD.begin(SD_CS)) {
+        for (;;) {
+            Serial.println(
+                "SD card initialization failed"
             );
             blink(LED);
         }
