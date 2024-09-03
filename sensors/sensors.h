e02c87e222fb36afbaa41e1eebfc00b1859afe69
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-03 21:07:27 +0530

    feat(sensors): error handling

---
+++ sensors/sensors.h
@@ -0,0 +1,14 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+enum class ErrCode {
+    SUCCESS,
+    FAIL,
+    TIMEOUT,
+    INVALIE_READING,
+    FILE_OPEN_FAILED,
+    WRITE_FAILED,
+    READ_FAILED
+};
+
+#endif // !SENSORS_H
