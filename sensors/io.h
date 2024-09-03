31787e3c27ea6be4255cc3d91d76e975b27bb794
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-03 21:05:31 +0530

    refactor(io): moved the IO (SD) to sensors

--- sensors/io.h
+++ sensors/io.h
@@ -1,3 +1,14 @@
+/**
+* Sensor configuration
+*
+*   SD Card reader
+*       - CS   = 5
+*       - MOSI = 23
+*       - MISO = 19
+*       - SCK  = 18
+*
+**/
+
 #ifndef IO_H
 #define IO_H
 
@@ -6,40 +17,43 @@
 #include <cstring>
 #include <SPI.h>
 #include <SD.h>
+#include "sensors.h"
+
+#define SD_CS 5
 
 class IO {
 public:
-    enum ErrCode {
-        SUCCESS = 0,
-        FILE_OPEN_FAILED,
-        WRITE_FAILED,
-        READ_FAILED
-    };
+    static ErrCode init() {
+        if (!SD.begin(SD_CS)) {
+            return ErrCode::FAIL;
+        }
+        return ErrCode::SUCCESS;
+    }
 
     static ErrCode write(const char *path, const char *payload) {
         File file = SD.open(path, FILE_WRITE);
         if (!file) {
-            return FILE_OPEN_FAILED;
+            return ErrCode::FILE_OPEN_FAILED;
         }
         file.println(payload);
         file.close();
-        return SUCCESS;
+        return ErrCode::SUCCESS;
     }
 
     static ErrCode read(const char *path, String &content) {
         File file = SD.open(path);
         if (!file) {
-            return FILE_OPEN_FAILED;
+            return ErrCode::FILE_OPEN_FAILED;
         }
 
         if (!file.available()) {
             file.close();
-            return READ_FAILED;
+            return ErrCode::READ_FAILED;
         }
 
         content = file.readStringUntil('\n');
         file.close();
-        return SUCCESS;
+        return ErrCode::SUCCESS;
     }
 };
 
