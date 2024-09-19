f72c1f8ee135ea5e7e75bbda01179e385116a233
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-19 12:53:36 +0530

    chore(refactor): formated the file

--- sensors/io.h
+++ sensors/io.h
@@ -1,60 +1,60 @@
 /**
-* Sensor configuration
-*
-*   SD Card reader
-*       - CS   = 5
-*       - MOSI = 23
-*       - MISO = 19
-*       - SCK  = 18
-*
-**/
+ * Sensor configuration
+ *
+ *   SD Card reader
+ *       - CS   = 5
+ *       - MOSI = 23
+ *       - MISO = 19
+ *       - SCK  = 18
+ *
+ **/
 
 #ifndef IO_H
 #define IO_H
 
+#include "sensors.h"
 #include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
 #include <climits>
 #include <cstring>
-#include <SPI.h>
-#include <SD.h>
-#include "sensors.h"
 
 #define SD_CS 5
 
 class IO {
 public:
-    static ErrCode init() {
-        if (!SD.begin(SD_CS)) {
-            return ErrCode::FAIL;
-        }
-        return ErrCode::SUCCESS;
+  static ErrCode init() {
+    if (!SD.begin(SD_CS)) {
+      return ErrCode::FAIL;
+    }
+    return ErrCode::SUCCESS;
+  }
+
+  static ErrCode write(const char *path, const char *payload) {
+    File file = SD.open(path, FILE_WRITE);
+    if (!file) {
+      return ErrCode::FILE_OPEN_FAILED;
+    }
+    file.println(payload);
+    file.close();
+    return ErrCode::SUCCESS;
+  }
+
+  static ErrCode read(const char *path, String &content) {
+    File file = SD.open(path);
+    if (!file) {
+      return ErrCode::FILE_OPEN_FAILED;
     }
 
-    static ErrCode write(const char *path, const char *payload) {
-        File file = SD.open(path, FILE_WRITE);
-        if (!file) {
-            return ErrCode::FILE_OPEN_FAILED;
-        }
-        file.println(payload);
-        file.close();
-        return ErrCode::SUCCESS;
+    if (!file.available()) {
+      file.close();
+      return ErrCode::READ_FAILED;
     }
 
-    static ErrCode read(const char *path, String &content) {
-        File file = SD.open(path);
-        if (!file) {
-            return ErrCode::FILE_OPEN_FAILED;
-        }
-
-        if (!file.available()) {
-            file.close();
-            return ErrCode::READ_FAILED;
-        }
-
-        content = file.readStringUntil('\n');
-        file.close();
-        return ErrCode::SUCCESS;
-    }
+    content = file.readStringUntil('\n');
+    file.close();
+    return ErrCode::SUCCESS;
+  }
 };
 
 #endif // !IO_H
--- sensors/ultrasonic.h
+++ sensors/ultrasonic.h
@@ -1,18 +1,18 @@
 /**
-* Sensor configuration
-*
-*   Ultrasonic Sensor
-*       - Echo = 12
-*       - Trig = 13
-*
-**/
+ * Sensor configuration
+ *
+ *   Ultrasonic Sensor
+ *       - Echo = 12
+ *       - Trig = 13
+ *
+ **/
 
 #ifndef ULTRASONIC_H
 #define ULTRASONIC_H
 
-#include <Arduino.h>
 #include "esp32-hal-gpio.h"
 #include "sensors.h"
+#include <Arduino.h>
 
 #define ECHO 12
 #define TRIG 13
@@ -22,43 +22,39 @@
 // Sensor code provided for the ultrasonic sensor
 #define SENSOR_CODE 0
 
-
 class Ultrasonic {
 private:
-    static long duration;
-    static float distance;
+  static long duration;
+  static float distance;
 
 public:
-    static void init() {
-        pinMode(TRIG, OUTPUT);
-        pinMode(ECHO, INPUT);
-        duration = 0;
-        distance = 0;
+  static void init() {
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+    duration = 0;
+    distance = 0;
+  }
+
+  static ErrCode read(String &value) {
+    digitalWrite(TRIG, LOW);
+    vTaskDelay(pdMS_TO_TICKS(2));
+    digitalWrite(TRIG, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(10));
+    digitalWrite(TRIG, LOW);
+
+    duration = pulseIn(ECHO, HIGH);
+    distance = duration * SOUND_SPEED / 2; // Distance in cm
+
+    if (!value.isEmpty()) {
+      value += "|";
     }
 
-    static ErrCode read(
-        String &value
-    ) {
-        digitalWrite(TRIG, LOW);
-        vTaskDelay(pdMS_TO_TICKS(2));
-        digitalWrite(TRIG, HIGH);
-        vTaskDelay(pdMS_TO_TICKS(10));
-        digitalWrite(TRIG, LOW);
-
-        duration = pulseIn(ECHO, HIGH);
-        distance = duration * SOUND_SPEED / 2; // Distance in cm
-
-        if (!value.isEmpty()) {
-            value += "|";
-        }
-
-        value += String(SENSOR_CODE) + ":" + String(distance, 2);
-        return ErrCode::SUCCESS;
-    }
+    value += String(SENSOR_CODE) + ":" + String(distance, 2);
+    return ErrCode::SUCCESS;
+  }
 };
 
 #endif // !ULTRASONIC_H
-
 
 long Ultrasonic::duration = 0;
 float Ultrasonic::distance = 0;
